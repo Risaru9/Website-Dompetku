561f36367bd58aa2ba56cdd8a9d0bbec4f70bdf0
Risaru9 <rizalmahardi109@gmail.com>
2026-01-21 08:32:41 +0700

    Fix API

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,21 +1,14 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-// --- PERBAIKAN 1: URL DINAMIS ---
-// Gunakan Environment Variable. Jika di Vercel, dia pakai link Vercel. 
-// Jika di laptop (dev), dia baru pakai localhost.
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000"; 
+const BASE_URL = "https://dompetkuapi.vercel.app/api";
 
 export const api = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
-  // --- PERBAIKAN 2: MATIKAN CREDENTIALS ---
-  // Karena backend kita setting "origin: '*'" (Boleh semua), 
-  // maka browser MELARANG withCredentials: true.
-  // Kita ubah jadi false agar tidak kena blokir CORS.
-  withCredentials: false, 
+  withCredentials: false,
 });
 
 api.interceptors.request.use(
@@ -23,16 +16,15 @@ api.interceptors.request.use(
     // 1. Coba cari di Cookie dulu
     let token = Cookies.get("token");
 
-    // 2. Kalau di Cookie kosong, CARI DI LOCALSTORAGE (Backup)
+    // 2. Kalau di Cookie kosong, CARI DI LOCALSTORAGE
     if (!token && typeof window !== "undefined") {
       token = localStorage.getItem("token") || undefined;
     }
 
-    // LOG DETEKTIF (Bagus untuk debugging)
+    // LOG DETEKTIF
     console.log(`[API REQUEST] Ke: ${config.url}`);
     
     if (token) {
-        // console.log(`[TOKEN FOUND] Token siap dikirim!`); // Opsional biar console bersih
         config.headers.Authorization = `Bearer ${token}`;
     } else {
         console.log(`[TOKEN MISSING] User belum login / Token tidak ada.`);
